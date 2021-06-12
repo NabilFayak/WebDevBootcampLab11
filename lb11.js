7092439c9668a42c18a81e6c7c23f2c9b61a1bd8
Nabil Fayak <72051470+NabilFayak@users.noreply.github.com>
2021-06-12 18:55:55 +0000

    updated fortune teller

--- lb11.js
+++ lb11.js
@@ -1,7 +1,33 @@
 function fortune() {
-    var children = window.prompt("How many children do you want?")
-    var partner = window.prompt("What's you partner's name?")
-    var location = window.prompt("What's your ideal place to live?")
-    var job = window.prompt("What job do you want?")
-    document.getElementById("demo").innerHTML = "You will have " + children + " children while married to " + partner + ". Your family will live happily in " + location + " and you will work as a " + job + ".";
+  var children = document.forms["myForm"]["childrenAmount"].value;
+  var partner = document.forms["myForm"]["partnerName"].value;
+  var location = document.forms["myForm"]["idealLocation"].value;
+  var job = document.forms["myForm"]["idealJob"].value;
+
+  if (children == "" && partner == "" && location == "" && job == "") {
+    alert("All fields must be filled out");
+    return false;
+  }
+
+  if (children == "") {
+    alert("Amount of children must be filled out");
+    return false;
+  }
+  
+  if (partner == "") {
+    alert("Partner's name must be filled out");
+    return false;
+  }
+  
+  if (location == "") {
+    alert("Ideal location must be filled out");
+    return false;
+  }
+  
+  if (job == "") {
+    alert("Ideal job must be filled out");
+    return false;
+  }
+
+  document.getElementById("fortuneteller").innerHTML = "You will have " + children + " children while married to " + partner + ". Your family will live happily in " + location + " and you will work as a " + job + ".";
 }
